db0e3c9b0a6fe7a8d8484fb574da46ddc5297c0d
Puneet Pandoh <ppandoh@gmail.com>
2016-03-06 00:46:53 -0800

    First commit

--- eztrac-ios-swift-app/ViewController.swift
+++ eztrac-ios-swift-app/ViewController.swift
@@ -2,14 +2,18 @@
 //  ViewController.swift
 //  eztrac-ios-swift-app
 //
-//  Created by Puneet Pandoh on 3/5/16.
+//  Created by Omnipara on 3/5/16.
 //  Copyright © 2016 Omnipara. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView?
+    var beacons: [CLBeacon]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +27,51 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            if(beacons != nil) {
+                return beacons!.count
+            } else {
+                return 0
+            }
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell:UITableViewCell? =
+            tableView.dequeueReusableCellWithIdentifier("MyIdentifier") as UITableViewCell?
+            
+            if(cell == nil) {
+                cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyIdentifier")
+                cell!.selectionStyle = UITableViewCellSelectionStyle.None
+            }
+            
+            let beacon:CLBeacon = beacons![indexPath.row]
+            var proximityLabel:String! = ""
+            
+            switch beacon.proximity {
+            case CLProximity.Far:
+                proximityLabel = "Far"
+            case CLProximity.Near:
+                proximityLabel = "Near"
+            case CLProximity.Immediate:
+                proximityLabel = "Immediate"
+            case CLProximity.Unknown:
+                proximityLabel = "Unknown"
+            }
+            
+            cell!.textLabel!.text = proximityLabel
+            
+            let detailLabel:String = "Major: \(beacon.major.integerValue), " +
+                "Minor: \(beacon.minor.integerValue), " +
+                "RSSI: \(beacon.rssi as Int)"
+            cell!.detailTextLabel!.text = detailLabel
+            
+            return cell!
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
